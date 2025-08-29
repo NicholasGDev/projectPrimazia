59b964a350a477ddccea3b2273b3f30542dc96c5
Nicholas G Orlando <nicholas.goulart1@gmail.com>
2025-08-28 22:39:22 -0300

    arruma home

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -2,7 +2,7 @@
   <div class="home-hero-root">
     <div class="home-hero-bg">
       <div class="home-hero-content">
-        <h1>Bem-vindo à Primazia Contabilidade</h1>
+        <h1>Primazia Contabilidade</h1>
         <p>Soluções contábeis para o seu negócio.</p>
       </div>
     </div>
